04924eac0afd2c4d63da1ec12e0baf559f2c7720
SnailMann <453001674@qq.com>
2019-02-15 18:58:29 +0800

    增加招聘模块

---
+++ tensquare_recruit/src/main/java/com/snailmann/tensquare/recruit/dao/EnterpriseDao.java
@@ -0,0 +1,14 @@
+package com.snailmann.tensquare.recruit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.snailmann.tensquare.recruit.pojo.Enterprise;
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface EnterpriseDao extends JpaRepository<Enterprise,String>,JpaSpecificationExecutor<Enterprise>{
+	
+}
---
+++ tensquare_recruit/src/main/java/com/snailmann/tensquare/recruit/dao/RecruitDao.java
@@ -0,0 +1,14 @@
+package com.snailmann.tensquare.recruit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.snailmann.tensquare.recruit.pojo.Recruit;
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface RecruitDao extends JpaRepository<Recruit,String>,JpaSpecificationExecutor<Recruit>{
+	
+}
