58c47d34ee67f201244ef36238904692ea7aee39
SnailMann <453001674@qq.com>
2019-08-08 17:44:10 +0800

    升级springboot到2.1.6,springcloud到Greenwich.SR2

--- tensquare_article/src/main/java/com/snailmann/tensquare/article/ArticleApplication.java
+++ tensquare_article/src/main/java/com/snailmann/tensquare/article/ArticleApplication.java
@@ -1,12 +1,17 @@
 package com.snailmann.tensquare.article;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ArticleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArticleApplication.class, args);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+
 }
