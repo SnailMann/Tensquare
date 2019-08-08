2a57ec2d01c317e9c013577e602d0a759b453862
SnailMann <453001674@qq.com>
2019-08-08 16:39:01 +0800

    添加Spring Cloud体系，初始加入Eureka

---
+++ tensquare_cloud/cloud_eureka/src/main/java/com/snailmann/cloud/eureka/CloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.snailmann.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaApplication.class, args);
+	}
+
+}
